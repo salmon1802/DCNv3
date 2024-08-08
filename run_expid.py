efbe14bdcf69877e8a9a8e9bf771e87cbcbdc542
salmon1802 <757635525@qq.com>
2024-08-08 17:28:48 +0800

    Model name update

--- run_expid.py
+++ run_expid.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, default='./config/', help='The config directory.')
-    parser.add_argument('--expid', type=str, default='SDCNv3_Criteo', help='The experiment id to run.')
+    parser.add_argument('--expid', type=str, default='DCNv3_Criteo', help='The experiment id to run.')
     parser.add_argument('--gpu', type=int, default=0 , help='The gpu index, -1 for cpu')
     args = vars(parser.parse_args())
     
